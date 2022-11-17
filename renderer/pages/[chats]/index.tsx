a7baa555470d26a16e46964a58168916f684bc89
Doug Hooker <DouglassHooker@gmail.com>
2022-11-16 16:51:23 -0800

    Init nextron project

---
+++ renderer/pages/[chats]/index.tsx
@@ -0,0 +1,18 @@
+import React from "react"
+import Head from "next/head";
+import {Box} from "@material-ui/core";
+
+const ChatRoot = () => {
+  return (
+      <React.Fragment>
+        <Head>
+          <title>Capybara Chat - Select a chat</title>
+        </Head>
+        <Box>
+          Hello
+        </Box>
+      </React.Fragment>
+  )
+}
+
+export default ChatRoot;
